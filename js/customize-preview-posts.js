2f9a57ce546a9a81dfbaccf460da1220c1154acb
westonruter <westonruter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-11 13:26:42 +0000

    Update to commit 052950c from https://github.com/xwp/wp-customize-posts.git
    git-svn-id: https://plugins.svn.wordpress.org/customize-posts/tags/0.2.2@1042690 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/customize-preview-posts.js
@@ -0,0 +1,20 @@
+/*global wp, _wpCustomizePreviewPostsData */
+( function ( api ) {
+
+	var OldPreview, preview;
+
+	// @todo Core really needs to not make the preview a private variable
+	OldPreview = api.Preview;
+	api.Preview = OldPreview.extend( {
+		initialize: function( params, options ) {
+			preview = this;
+
+			preview.bind( 'active', function() {
+				preview.send( 'customize-posts', _wpCustomizePreviewPostsData );
+			} );
+
+			OldPreview.prototype.initialize.call( this, params, options );
+		}
+	} );
+
+} )( wp.customize );
